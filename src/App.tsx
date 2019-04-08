3a5b46e6fc9fc031114a1680ccf96d393e1a4d83
Javier Pascual <javipascualg@gmail.com>
2019-04-08 09:07:22 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import {
   IonButton,
   IonContent,
 } from '@ionic/react';
+import * as firebase from 'firebase';
 
+const servers = {
+  'iceServers': [
+    {'urls': 'stun:stun.services.mozilla.com'},
+    {'urls': 'stun:stun.l.google.com:19302'},
+    {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}
+  ]
+};
 
 interface State {
   inCall: boolean;
@@ -15,41 +23,90 @@ interface State {
 class App extends Component<{}, State> {
   pc1?: RTCPeerConnection;
   pc2?: RTCPeerConnection;
+  pc: RTCPeerConnection;
   localStream?: MediaStream;
+  database: firebase.database.Reference;
+  id: number;
   
   constructor(props: {}) {
     super(props);
+
+    this.id = Math.floor(Math.random()*1000000000);
+    this.pc = new RTCPeerConnection(servers);
+
+    const cfg = {
+      apiKey: "AIzaSyAathUls5mxSGQVfObJJj3_1Gsqy5D5mtI",
+      authDomain: "testwebrtc-90120.firebaseapp.com",
+      databaseURL: "https://testwebrtc-90120.firebaseio.com",
+      projectId: "testwebrtc-90120",
+      storageBucket: "testwebrtc-90120.appspot.com",
+      messagingSenderId: "914167919631"
+    };
+
+    firebase.initializeApp(cfg);
+    this.database = firebase.database().ref();
+    this.database.on('child_added', this.readMessage);
 
     this.state = { inCall: false }
     this.call = this.call.bind(this);
     this.hangUp = this.hangUp.bind(this);
 
     this.onIceCandidate = this.onIceCandidate.bind(this);
-    this.onCreateOfferSuccess = this.onCreateOfferSuccess.bind(this);
     this.onCreateSessionDescriptionError = this.onCreateSessionDescriptionError.bind(this);
-
-    this.onSetLocalSuccess = this.onSetLocalSuccess.bind(this);
     this.onSetSessionDescriptionError = this.onSetSessionDescriptionError.bind(this);
 
     this.getName = this.getName.bind(this);
     this.getOtherPc = this.getOtherPc.bind(this);
+
+    this.sendMessage = this.sendMessage.bind(this);
+    this.readMessage = this.readMessage.bind(this);
   }
 
   async componentDidMount() {
+    this.pc.addEventListener('icecandidate', e => {
+      e.candidate ? this.sendMessage(this.id, JSON.stringify({'ice': e.candidate})) : console.log("Sent All Ice");
+    });
+
+    // this.pc.addEventListener('icecandidate', e => {
+    //   e.candidate ? this.sendMessage(this.id, JSON.stringify({'ice': e.candidate})) : console.log("Sent All Ice");
+    // });
+
+
+
     console.log('Requesting local stream');
 
     try {
       const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
       console.log('Received local stream');
 
-      const localVideo = document.querySelector('#localVideo') as HTMLVideoElement;
-
-      localVideo!.srcObject = stream;
       this.localStream = stream;
+  
     } catch (e) {
       alert(`getUserMedia() error: ${e.name}`);
     }
   }
+  
+  sendMessage(sender: number, message: string) {
+    var msg = this.database.push({ sender, message });
+    msg.remove();
+  }
+
+  async readMessage(data: any) {
+    var msg = JSON.parse(data.val().message);
+    var sender = data.val().sender;
+    if (sender != this.id) {
+        if (msg.ice != undefined) {
+          this.pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        } else if (msg.sdp.type == "offer") {
+          await this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+          const answer = await this.pc.createAnswer()
+          await this.pc.setLocalDescription(answer);
+          this.sendMessage(this.id, JSON.stringify({'sdp': this.pc.localDescription}));
+        } else if (msg.sdp.type == "answer") {
+          this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+        }
+    }
+};
 
   getName(pc: RTCPeerConnection) {
     return (pc === this.pc1) ? 'pc1' : 'pc2';
@@ -62,44 +119,38 @@ class App extends Component<{}, State> {
   async call() {
     console.log('Starting call');
 
-    const videoTracks = this.localStream!.getVideoTracks();
-    const audioTracks = this.localStream!.getAudioTracks();
-  
-    if (videoTracks.length > 0) {
-      console.log(`Using video device: ${videoTracks[0].label}`);
-    }
+    // add local video
+    const localVideo = document.querySelector('#localVideo') as HTMLVideoElement;
+    localVideo.srcObject = this.localStream!;
+    // publish local video and audio
+    this.localStream!.getTracks().forEach(t => this.pc.addTrack(t));
 
-    if (audioTracks.length > 0) {
-      console.log(`Using audio device: ${audioTracks[0].label}`);
-    }
-
-    const configuration = {};
-    console.log('RTCPeerConnection configuration:', configuration);
-    this.pc1 = new RTCPeerConnection(configuration);
-    console.log('Created local peer connection object pc1');
-    this.pc1.addEventListener('icecandidate', e => this.onIceCandidate(this.pc1!, e));
-    this.pc2 = new RTCPeerConnection(configuration);
-    console.log('Created remote peer connection object pc2');
-    this.pc2.addEventListener('icecandidate', e => this.onIceCandidate(this.pc2!, e));
-    this.pc1.addEventListener('iceconnectionstatechange', e => this.onIceStateChange(this.pc1!, e as RTCPeerConnectionIceEvent));
-    this.pc2.addEventListener('iceconnectionstatechange', e => this.onIceStateChange(this.pc2!, e as RTCPeerConnectionIceEvent));
-    this.pc2.addEventListener('track', this.gotRemoteStream);
+    // send offer
+    const offer = await this.pc.createOffer();
+    this.pc.setLocalDescription(offer)
+    // this.pc1 = new RTCPeerConnection();
+    // console.log('Created local peer connection object pc1');
+    // this.pc1.addEventListener('icecandidate', e => this.onIceCandidate(this.pc1!, e));
+    // this.pc2 = new RTCPeerConnection();
+    // console.log('Created remote peer connection object pc2');
+    // this.pc2.addEventListener('icecandidate', e => this.onIceCandidate(this.pc2!, e));
+    // this.pc2.addEventListener('track', this.gotRemoteStream);
   
-    this.localStream!.getTracks().forEach(track => this.pc1!.addTrack(track, this.localStream!));
-    console.log('Added local stream to pc1');
+    // this.localStream!.getTracks().forEach(track => this.pc1!.addTrack(track, this.localStream!));
+    // console.log('Added local stream to pc1');
   
-    try {
-      console.log('pc1 createOffer start');
-      const offer = await this.pc1.createOffer({
-        offerToReceiveAudio: true,
-        offerToReceiveVideo: true
-      });
-      await this.onCreateOfferSuccess(offer);
-      this.setState({ inCall: true });
-    }
-    catch (e) {
-      this.onCreateSessionDescriptionError(e);
-    }
+    // try {
+    //   console.log('pc1 createOffer start');
+    //   const offer = await this.pc1.createOffer({
+    //     offerToReceiveAudio: true,
+    //     offerToReceiveVideo: true
+    //   });
+    //   await this.onCreateOfferSuccess(offer);
+    //   this.setState({ inCall: true });
+    // }
+    // catch (e) {
+    //   this.onCreateSessionDescriptionError(e);
+    // }
   }
 
   onCreateSessionDescriptionError(error: Error) {
@@ -111,7 +162,6 @@ class App extends Component<{}, State> {
     console.log('pc1 setLocalDescription start');
     try {
       await this.pc1!.setLocalDescription(desc);
-      this.onSetLocalSuccess(this.pc1!);
     }
     catch (e) {
       this.onSetSessionDescriptionError(e);
@@ -120,7 +170,6 @@ class App extends Component<{}, State> {
     console.log('pc2 setRemoteDescription start');
     try {
       await this.pc2!.setRemoteDescription(desc);
-      this.onSetRemoteSuccess(this.pc2!);
     }
     catch (e) {
       this.onSetSessionDescriptionError(e);
@@ -137,15 +186,7 @@ class App extends Component<{}, State> {
       this.onCreateSessionDescriptionError(e);
     }
   }
-  
-  onSetLocalSuccess(pc: RTCPeerConnection) {
-    console.log(`${this.getName(pc)} setLocalDescription complete`);
-  }
-  
-  onSetRemoteSuccess(pc: RTCPeerConnection) {
-    console.log(`${this.getName(pc)} setRemoteDescription complete`);
-  }
-  
+
   onSetSessionDescriptionError(error: Error) {
     console.log(`Failed to set session description: ${error.toString()}`);
   }
@@ -164,14 +205,12 @@ class App extends Component<{}, State> {
     console.log('pc2 setLocalDescription start');
     try {
       await this.pc2!.setLocalDescription(desc);
-      this.onSetLocalSuccess(this.pc2!);
     } catch (e) {
       this.onSetSessionDescriptionError(e);
     }
     console.log('pc1 setRemoteDescription start');
     try {
       await this.pc1!.setRemoteDescription(desc);
-      this.onSetRemoteSuccess(this.pc1!);
     } catch (e) {
       this.onSetSessionDescriptionError(e);
     }
@@ -180,27 +219,16 @@ class App extends Component<{}, State> {
   async onIceCandidate(pc: RTCPeerConnection, event: RTCPeerConnectionIceEvent) {
     try {
       await (this.getOtherPc(pc)!.addIceCandidate(event.candidate!));
-      this.onAddIceCandidateSuccess(pc);
     } catch (e) {
       this.onAddIceCandidateError(pc, e);
     }
     console.log(`${this.getName(pc)} ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
   }
-  
-  onAddIceCandidateSuccess(pc: RTCPeerConnection) {
-    console.log(`${this.getName(pc)} addIceCandidate success`);
-  }
-  
- onAddIceCandidateError(pc: RTCPeerConnection, error: Error) {
+
+  onAddIceCandidateError(pc: RTCPeerConnection, error: Error) {
     console.log(`${this.getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
   }
-  
-  onIceStateChange(pc: RTCPeerConnection, event: RTCPeerConnectionIceEvent) {
-    if (pc) {
-      console.log(`${this.getName(pc)} ICE state: ${pc.iceConnectionState}`);
-      console.log('ICE state change event: ', event);
-    }
-  }
+
   
   hangUp() {
     console.log('Ending call');
